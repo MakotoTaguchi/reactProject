b819d673d439785fff229f9610ac53e45203da0f
MakotoTaguchi <taguchimakoto@icloud.com>
2022-05-12 22:32:43 +0900

    main

---
+++ twitter-clone-udemy/src/components/timeline/Timeline.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Timeline.css";
+
+function Timeline() {
+  return (
+    <div className="timeline">
+      {/* Header */}
+      <div className="timeline_header">
+        <h2>ホーム</h2>
+      </div>
+
+      {/* TweetBox */}
+
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+    </div>
+  );
+}
+
+export default Timeline;
