66f19d022fe5ff5599f2e039bb2c6cf4fcf0ea49
MakotoTaguchi <taguchimakoto@icloud.com>
2022-05-12 15:07:55 +0900

    main

---
+++ twitter-clone-udemy/src/components/sidebar/Sidebar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      {/* ツイッターアイコン */}
+
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+
+      {/* ツイートボタン */}
+    </div>
+  );
+}
+
+export default Sidebar;
