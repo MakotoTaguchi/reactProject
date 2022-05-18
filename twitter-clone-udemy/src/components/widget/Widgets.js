9c9cad057cfb23ac5c137d86fe82bc0e310d61b8
MakotoTaguchi <taguchimakoto@icloud.com>
2022-05-18 19:50:46 +0900

    main

--- twitter-clone-udemy/src/components/widget/Widgets.js
+++ twitter-clone-udemy/src/components/widget/Widgets.js
@@ -3,15 +3,9 @@ import React from "react";
 import {
   TwitterTimelineEmbed,
   TwitterShareButton,
-  TwitterFollowButton,
-  TwitterHashtagButton,
-  TwitterMentionButton,
   TwitterTweetEmbed,
-  TwitterMomentShare,
-  TwitterDMButton,
-  TwitterVideoEmbed,
-  TwitterOnAirButton,
 } from "react-twitter-embed";
+import "./Widgets.css";
 
 function Widgets() {
   return (
