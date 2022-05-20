f20b057d2da68e73a0a53fd0d7f5b9c02afa3c04
MakotoTaguchi <taguchimakoto@icloud.com>
2022-05-20 16:43:59 +0900

    main

--- twitter-clone-udemy/src/components/timeline/Post.js
+++ twitter-clone-udemy/src/components/timeline/Post.js
@@ -1,5 +1,5 @@
 import { Avatar } from "@mui/material";
-import React from "react";
+import React, { forwardRef } from "react";
 import {
   ChatBubbleOutline,
   FavoriteBorder,
@@ -8,36 +8,39 @@ import {
   VerifiedUser,
 } from "@mui/icons-material";
 import "./Post.css";
-function Post({ displayName, username, verified, text, image, avatar }) {
-  return (
-    <div className="post">
-      <div className="post_avatar">
-        <Avatar src={avatar} />
-      </div>
-      <div className="post_body">
-        <div className="poat_header">
-          <div className="post_headerText">
-            <h3>
-              {displayName}
-              <span className="post_headerSpecial">
-                <VerifiedUser className="post_badge" />@{username}
-              </span>
-            </h3>
+
+const Post = forwardRef(
+  ({ displayName, username, verified, text, image, avatar }, ref) => {
+    return (
+      <div className="post" ref={ref}>
+        <div className="post_avatar">
+          <Avatar src={avatar} />
+        </div>
+        <div className="post_body">
+          <div className="poat_header">
+            <div className="post_headerText">
+              <h3>
+                {displayName}
+                <span className="post_headerSpecial">
+                  <VerifiedUser className="post_badge" />@{username}
+                </span>
+              </h3>
+            </div>
+            <div className="post_headerDescription">
+              <p>{text}</p>
+            </div>
           </div>
-          <div className="post_headerDescription">
-            <p>{text}</p>
+          <img src={image} />
+          <div className="post_footer">
+            <ChatBubbleOutline fontSize="small" />
+            <Repeat fontSize="small" />
+            <FavoriteBorder fontSize="small" />
+            <PublishOutlined fontSize="small" />
           </div>
         </div>
-        <img src={image} />
-        <div className="post_footer">
-          <ChatBubbleOutline fontSize="small" />
-          <Repeat fontSize="small" />
-          <FavoriteBorder fontSize="small" />
-          <PublishOutlined fontSize="small" />
-        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+);
 
 export default Post;
