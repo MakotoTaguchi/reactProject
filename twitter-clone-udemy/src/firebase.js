1f7c86c7a1875e2ac18c72e1ec678ac86db4fe69
MakotoTaguchi <taguchimakoto@icloud.com>
2022-05-20 14:58:23 +0900

    main

---
+++ twitter-clone-udemy/src/firebase.js
@@ -0,0 +1,16 @@
+import { initializaApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCwbmgrDH3S_uTLzCRtODu6YEFhdq_b7hQ",
+  authDomain: "twitter-clone-udemy-dc1e5.firebaseapp.com",
+  projectId: "twitter-clone-udemy-dc1e5",
+  storageBucket: "twitter-clone-udemy-dc1e5.appspot.com",
+  messagingSenderId: "512344933227",
+  appId: "1:512344933227:web:9403f55f5b40993eca422e",
+};
+
+const app = initializaApp(firebaseConfig);
+const db = getFirestore();
+
+export default db;
