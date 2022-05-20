d5b8d0daaffd8563468625b1585e3cb95402e9e5
MakotoTaguchi <taguchimakoto@icloud.com>
2022-05-20 16:44:03 +0900

    main

--- twitter-clone-udemy/src/components/timeline/Timeline.js
+++ twitter-clone-udemy/src/components/timeline/Timeline.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Post from "./Post";
 import "./Timeline.css";
 import TweetBox from "./TweetBox";
+import db from "../../firebase";
+import {
+  collection,
+  getDocs,
+  onSnapshot,
+  orderBy,
+  query,
+  QuerySnapshot,
+} from "firebase/firestore";
+import FlipMove from "react-flip-move";
 
 function Timeline() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const postData = collection(db, "posts");
+    const q = query(postData, orderBy("timestamp", "desc"));
+    // getDocs(q).then((querySnapshot) => {
+    //   setPosts(querySnapshot.docs.map((doc) => doc.data()));
+    // });
+
+    // リアルタイムでデータを取得
+    onSnapshot(q, (querySnapshot) => {
+      setPosts(querySnapshot.docs.map((doc) => doc.data()));
+    });
+  }, []);
+
   return (
     <div className="timeline">
       {/* Header */}
@@ -15,18 +40,19 @@ function Timeline() {
       <TweetBox />
 
       {/* Post */}
-      <Post
-        displayName="プログラミングチュートリアル"
-        username="Shin_Engineer"
-        verified={true}
-        text="初めてのツイート"
-        avatar="http://shincode.info/wp-content/uploads/2021/12/icon.png"
-        image="https://source.unsplash.com/random"
-      />
-      {/* Post */}
-      {/* Post */}
-      {/* Post */}
-      {/* Post */}
+      <FlipMove>
+        {posts.map((post) => (
+          <Post
+            key={post.text}
+            displayName={post.displayName}
+            username={post.username}
+            verified={post.verified}
+            text={post.text}
+            avatar={post.avatar}
+            image={post.image}
+          />
+        ))}
+      </FlipMove>
     </div>
   );
 }
